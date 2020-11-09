6f34dc67916e488adda6089e441eb6ac70067f40
devenock <enockomondi305@gmail.com>
2020-11-09 15:00:05 -0500

    Creating server

--- server.js
+++ server.js
@@ -6,8 +6,18 @@ const bodyParser = require('body-parser');
 // initialize express
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.set('view engine', 'ejs');
+
 // PORT
 const PORT = process.env.PORT || 3000;
+
+// ROUTES
+app.get('/', (req, res) => {
+    res.render('welcome');
+});
 
 // listen to the server
 app.listen(PORT, () => {
